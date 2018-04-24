ccf67e084986d19800807660d355027b5c440d1d
Katharine McLeod <katharinermcleod@gmail.com>
2018-04-24 14:27:27 -0400

    scratch knex queries

--- scratch/queries.js
+++ scratch/queries.js
@@ -18,3 +18,30 @@ knex
   .catch(err => {
     console.error(err);
   });
+
+let id = 1003;
+
+get by id
+
+knex('notes')
+  .select('notes.id', 'title', 'content')
+  .where('notes.id', id)
+  .then(results => console.log(results))
+  .catch (err => console.log(err));
+
+let newNote = {
+  title: 'Katharines new note',
+  content: 'it has content'
+};
+
+knex('notes')
+.insert(newNote)
+.returning(['notes.id', 'title', 'content'])
+.then(results => console.log(results));
+
+let id = 11;
+
+knex('notes')
+  .del()
+  .where('notes.id', id)
+  .then(results => console.log(results));
